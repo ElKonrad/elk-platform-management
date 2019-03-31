67bac4d3efdc27e757090cd61fd975750b0f0784
Vadym Borys <bublik.drdrdr@gmail.com>
2019-03-31 18:27:54 +0200

    adding new application front

---
+++ frontend/src/app/apps/apps-add/apps-add.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {MonitoredApplication} from "../../domain/request/monitored-application";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {UserResponse} from "../../domain/response/user-response";
+import {ErrorResponse} from "../../error/error-response";
+import {AppsService} from "../apps.service";
+
+@Component({
+    selector: 'app-apps-add',
+    templateUrl: './apps-add.component.html',
+    styleUrls: ['./apps-add.component.css']
+})
+export class AppsAddComponent implements OnInit {
+
+    addApplicationForm: FormGroup;
+    application: MonitoredApplication = new MonitoredApplication();
+    responseError: ErrorResponse;
+    responseStatus: number;
+    userResponse: UserResponse = new UserResponse();
+
+    constructor(private appsService: AppsService,
+                private fb: FormBuilder,
+                private router: Router) {
+    }
+
+    ngOnInit() {
+        this.addApplicationForm = this.fb.group({
+            name: ['', [Validators.required, Validators.pattern('^[a-z0-9_-\\s]+$')]],
+            filePath: ['', [Validators.required, Validators.pattern('(([^\\\\/]*[\\\\/])*)([^\\\\/]+)$')]]
+        });
+    }
+
+    addApp() {
+        this.application = this.addApplicationForm.getRawValue();
+        this.appsService.addApplication(this.application)
+            .subscribe(
+                res => {
+                    this.addApplicationForm.reset();
+                    this.appsService.onAppAdded.emit(this.application);
+                },
+                err => {
+                    this.responseStatus = err.status;
+                    this.responseError = err.json();
+                });
+    }
+
+}
---
+++ frontend/src/app/apps/apps-list/apps-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {AppsService} from "../apps.service";
+import {MonitoredApplication} from "../../domain/request/monitored-application";
+import {Task} from "../../tasks/task.model";
+
+@Component({
+  selector: 'app-apps-list',
+  templateUrl: './apps-list.component.html',
+  styleUrls: ['./apps-list.component.css']
+})
+export class AppsListComponent implements OnInit {
+
+  apps: MonitoredApplication[];
+
+  constructor(private appsService: AppsService) { }
+
+  ngOnInit() {
+    this.appsService.getApplications().subscribe(
+        (res: any) => {
+          this.apps = res;
+        }
+    );
+      this.appsService.onAppAdded.subscribe(
+          (app: MonitoredApplication) =>  {
+            this.apps.push(app);
+          }
+      );
+  }
+
+}
